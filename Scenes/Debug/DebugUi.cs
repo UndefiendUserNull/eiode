786067faff49ea6f1f99a687f0ff16cbf5dceb61
UndefiendUserNull <viski8548@gmail.com>
2025-05-13 21:16:04 +0300

    Discord sdk test

--- Scenes/Debug/DebugUi.cs
+++ Scenes/Debug/DebugUi.cs
@@ -19,6 +19,7 @@ public partial class DebugUi : Control
     public override void _Process(double delta)
     {
         _sb.Clear();
+        _sb.Append($"FPS : {Engine.GetFramesPerSecond()}\n");
         _sb.Append($"Delta : {delta}\n");
         _sb.Append($"Time In Air : {_player._timeInAir}\n");
         _sb.Append($"Time Since Last Jump : {_player._timeSinceLastJumpInput}\n");
