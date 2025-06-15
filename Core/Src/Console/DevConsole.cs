c0ee532036070b24134fcf6321addf4e476dc21f
UndefiendUserNull <viski8548@gmail.com>
2025-06-15 15:23:40 +0300

    DevConsole improvements

--- Core/Src/Console/DevConsole.cs
+++ Core/Src/Console/DevConsole.cs
@@ -1,205 +1,200 @@
 using Godot;
 using EIODE.Utils;
 using EIODE.Scripts.Core;
-using System.Text;
 using System;
 using System.Collections.Generic;
 
 namespace EIODE.Core.Console;
 public partial class DevConsole : Control
 {
-    [Export] private int _currentHistoryIndex = 0;
-    private LineEdit _input = null;
-    private RichTextLabel _log = null;
-    private Game _game = null;
-    private AutoCompleter completer = null;
-    //private readonly StringBuilder _sb = new();
+    [Export] private int _currentHistoryIndex = -1;
+    private LineEdit _input = null!;
+    private RichTextLabel _log = null!;
+    private Game _game = null!;
+    private AutoCompleter _completer = null!;
     private readonly List<string> _history = [];
+
     public bool IsShown { get; private set; } = false;
 
     public override void _Ready()
     {
         _game = Game.GetGame(this);
-        if (!_game.InitSpawnConsole) return;
-        completer = new(ConsoleCommandSystem.GetCommands().Keys);
-        _input = GetChild<LineEdit>(1);
-        _log = GetChild<Panel>(0).GetChild<RichTextLabel>(0);
+        if (!_game.InitSpawnConsole)
+        {
+            QueueFree();
+            return;
+        }
+
+        _input = GetNode<LineEdit>("Input");
+        _log = GetNode<RichTextLabel>("Panel/Logger");
+
+        _completer = new AutoCompleter(ConsoleCommandSystem.GetCommands().Keys);
+
         ConsoleCommandSystem.RegisterInstance(this);
-        this.Hide();
-        _input.TextSubmitted += Input_TextSubmitted;
-        _input.TextChanged += Input_TextChanged;
+        Hide();
+
+        _input.TextSubmitted += OnTextSubmitted;
+        _input.TextChanged += OnTextChanged;
+
         _log.Clear();
         _log.PushFontSize(14);
         _log.PushOutlineSize(8);
         _log.PushOutlineColor(Color.Color8(0, 0, 0));
-        //_log.AppendText(_sb.ToString());
-    }
-
-    private void Input_TextChanged(string newText)
-    {
-        foreach (var suggestion in completer.GetSuggestions(newText))
-        {
-        }
     }
 
     public override void _ExitTree()
     {
-        _input.TextSubmitted -= Input_TextSubmitted;
-        _input.TextChanged -= Input_TextChanged;
+        _input.TextSubmitted -= OnTextSubmitted;
+        _input.TextChanged -= OnTextChanged;
     }
 
-    private void Input_TextSubmitted(string newText)
+    private void OnTextSubmitted(string text)
     {
-        if (IsShown)
+        if (!IsShown) return;
+
+        string command = text.Trim();
+        if (!string.IsNullOrEmpty(command))
         {
-            string command = _input.Text.Trim('\n');
-            if (!string.IsNullOrEmpty(command))
-            {
-                _game.Console?.Log(command, LogLevel.BLANK);
-                ConsoleCommandSystem.ExecuteCommand(command);
-                _history.Add(command);
-            }
-            _input.Clear();
+            Log(command, LogLevel.BLANK);
+            ConsoleCommandSystem.ExecuteCommand(command);
+            _history.Insert(0, command); // Insert at front for easy cycling
+            _currentHistoryIndex = -1;
         }
+        _input.Clear();
+    }
+
+    private void OnTextChanged(string text)
+    {
+        // TODO: Show auto-complete UI
     }
 
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed(InputHash.TOGGLE_CONSOLE))
         {
-            IsShown = !IsShown;
-
-            if (IsShown)
-            {
-                ShowConsole();
-                _input.Clear();
-                _input.GrabFocus();
-            }
-            else
-            {
-                _currentHistoryIndex = 0;
-                HideConsole();
-            }
+            ToggleConsole();
         }
+
+        if (!IsShown) return;
+
+        if (Input.IsActionJustPressed(InputHash.UP))
+        {
+            CycleHistory(1);
+        }
+        else if (Input.IsActionJustPressed(InputHash.DOWN))
+        {
+            CycleHistory(-1);
+        }
+        else if (Input.IsActionJustPressed(InputHash.K_TAB))
+        {
+            AutoComplete();
+        }
+    }
+
+    private void ToggleConsole()
+    {
+        IsShown = !IsShown;
+
         if (IsShown)
         {
-            if (Input.IsActionJustPressed(InputHash.UP))
-            {
-                if (_currentHistoryIndex + 1 < _history.Count)
-                    _currentHistoryIndex++;
-                else
-                    _currentHistoryIndex = 0;
-
-                _input.Text = _history[_currentHistoryIndex];
-            }
-            if (Input.IsActionJustPressed(InputHash.DOWN))
-            {
-                if (_currentHistoryIndex - 1 > 0)
-                    _currentHistoryIndex--;
-                else
-                    _currentHistoryIndex = _history.Count - 1;
-
-                if (_currentHistoryIndex >= 0)
-                    _input.Text = _history[_currentHistoryIndex];
-            }
-            if (Input.IsActionJustPressed(InputHash.K_TAB))
-            {
-                var suggestions = completer.GetSuggestions(_input.Text);
-
-                if (suggestions.Count > 0)
-                    _input.Text = suggestions[0];
-            }
+            ShowConsole();
+        }
+        else
+        {
+            HideConsole();
         }
     }
 
     private void ShowConsole()
     {
-        this.Show();
+        Show();
+        _input.Clear();
         _input.GrabFocus();
+        _currentHistoryIndex = -1;
         _game.GetPlayer().Lock();
         Game.ShowMouse();
     }
 
     private void HideConsole()
     {
-        this.Hide();
+        Hide();
         _input.ReleaseFocus();
         _game.GetPlayer().UnLock();
         Game.HideMouse();
     }
 
-    public void Log(string msg, LogLevel logLevel = LogLevel.INFO)
+    private void CycleHistory(int direction)
     {
-        switch (logLevel)
+        if (_history.Count == 0) return;
+
+        _currentHistoryIndex += direction;
+
+        if (_currentHistoryIndex < 0)
         {
-            case LogLevel.INFO:
-                Info(msg);
-                break;
-            case LogLevel.WARNING:
-                Warn(msg);
-                break;
-            case LogLevel.ERROR:
-                Error(msg);
-                break;
-            case LogLevel.BLANK:
-                BlankLog(msg);
-                break;
+            _currentHistoryIndex = 0;
+        }
+        else if (_currentHistoryIndex >= _history.Count)
+        {
+            _currentHistoryIndex = _history.Count - 1;
+        }
+
+        _input.Text = _history[_currentHistoryIndex];
+        _input.CaretColumn = _input.Text.Length;
+    }
+
+    private void AutoComplete()
+    {
+        var suggestions = _completer.GetSuggestions(_input.Text);
+        if (suggestions.Count > 0)
+        {
+            _input.Text = suggestions[0];
+            _input.CaretColumn = _input.Text.Length;
         }
     }
 
-    // Too lazy to change all the GD.Print to a Log one, DON'T USE THIS, Use Log instead
-    [Obsolete]
-    public void Print(string msg)
+    public void Log(string msg, LogLevel logLevel = LogLevel.INFO)
     {
-        Info(msg);
-    }
+        string line = logLevel switch
+        {
+            LogLevel.INFO => $"[color=green][INFO] : {msg}[/color]",
+            LogLevel.WARNING => $"[color=yellow][WARNING] : {msg}[/color]",
+            LogLevel.ERROR => $"[color=red][ERROR] : {msg}[/color]",
+            LogLevel.BLANK => msg,
+            _ => msg
+        };
 
-    // Too lazy to change all the GD.PushErr to a Log one, DON'T USE THIS, Use Log instead
-    [Obsolete]
-    public void PushErr(string msg)
-    {
-        Error(msg);
-    }
-    public enum LogLevel
-    {
-        INFO,
-        WARNING,
-        ERROR,
-        BLANK
-    }
-    public void BlankLog(string msg)
-    {
-        var line = $"{msg}";
-        AddLogLine(line);
-    }
-    public void Info(string msg)
-    {
-        var line = $"[color=green][INFO] : {msg}[/color]";
-        AddLogLine(line);
-
-    }
-    public void Warn(string msg)
-    {
-        var line = $"[color=yellow][WARNING] : {msg}[/color]";
         AddLogLine(line);
     }
 
-    public void Error(string msg)
-    {
-        var line = $"[color=red][ERROR] : {msg}[/color]";
-        AddLogLine(line);
-    }
     private void AddLogLine(string line)
     {
-        _log?.AppendText(line + '\n');
+        _log.AppendText(line + "\n");
     }
 
-    [ConsoleCommand("help", "Helps you :D")]
+    [ConsoleCommand("help", "Shows all available console commands.")]
     public void Help()
     {
         foreach (var cmd in ConsoleCommandSystem.GetCommands())
         {
             Log($"{cmd.Key}: {cmd.Value.Description}");
         }
+    }
+
+    [Obsolete("Use Log() instead.")]
+    public void Print(string msg) => Info(msg);
+
+    [Obsolete("Use Log() instead.")]
+    public void PushErr(string msg) => Error(msg);
+
+    public void Info(string msg) => Log(msg, LogLevel.INFO);
+    public void Warn(string msg) => Log(msg, LogLevel.WARNING);
+    public void Error(string msg) => Log(msg, LogLevel.ERROR);
+
+    public enum LogLevel
+    {
+        INFO,
+        WARNING,
+        ERROR,
+        BLANK
     }
 }
--- Utils/ScenesHash.cs
+++ Utils/ScenesHash.cs
@@ -6,8 +6,8 @@ namespace EIODE.Utils
     /// </summary>
     public static class ScenesHash
     {
-        public const string PLAYER_SCENE = "res://Scenes/Player/player.tscn";
-        public const string LEVEL_LOADER_SCENE = "res://Scripts/Core/level_loader.tscn";
-        public const string CONSOLE_SCENE = "res://Scenes/UI/console.tscn";
+        public const string PLAYER_SCENE = "res://Player/player.tscn";
+        public const string LEVEL_LOADER_SCENE = "res://Core/level_loader.tscn";
+        public const string CONSOLE_SCENE = "res://Core/console.tscn";
     }
 }
