b66bd7ee3158d64a69c2cbbfe29d529c2561dbc5
UndefiendUserNull <viski8548@gmail.com>
2025-06-22 02:12:44 +0300

    Custom console "prev"

--- Resources/Src/PlayerMovementSettings.cs
+++ Resources/Src/PlayerMovementSettings.cs
@@ -1,22 +1,28 @@
 using Godot;
-using System;
 
 namespace EIODE.Resources.Src;
 
 [GlobalClass]
 public partial class PlayerMovementSettings : Resource
 {
-    [Export] public float _sensitivity = 0.05f;
-    [Export] public float _acceleration = 5f;
-    [Export] public float _airControl = 2f;
-    [Export] public float _maxVelocityAir = 0.6f;
-    [Export] public float _maxVelocityGround = 6.0f;
+    // The initial values are the default values used for the latest commit
+
+    [Export] public float Sensitivity { get; set; } = 0.2f;
+    [Export] public float Acceleration { get; set; } = 10f;
+    [Export] public float AirControl { get; set; } = 5f;
+    [Export] public float GravityRampStart { get; set; } = 0.3f;
+    [Export] public float GravityRampMultiplier { get; set; } = 2.0f;
+    [Export] public float GravityMinScale { get; set; } = 1.0f;
+    [Export] public float GravityMaxScale { get; set; } = 3.0f;
+
+    [Export] public float MaxVelocityAir { get; set; } = 20f;
+    [Export] public float MaxVelocityGround { get; set; } = 22f;
     // Used in Init
-    [Export] public float _jumpModifier = 0.85f;
+    [Export] public float JumpModifier { get; set; } = 11.5f;
     // Used in Init
-    [Export] public float _gravity = 15.34f;
-    [Export] public float _jumpBufferingTime = 0.1f;
-    [Export] public float _coyoteTime = 0.15f;
+    [Export] public float Gravity { get; set; } = 110.0f;
+    [Export] public float JumpBufferingTime { get; set; } = 0.12f;
+    [Export] public float CoyoteTime { get; set; } = 0.15f;
 
     public PlayerMovementSettings() { }
 }
